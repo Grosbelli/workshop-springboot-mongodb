bd7ef083ec8be7622988d88c13cab08bb34737ee
Andrey Grosbelli <agrosbelli@yahoo.com.br>
2021-07-06 16:34:22 -0300

    Insercao de usuario com POST

--- src/main/java/com/andreygrosbelli/workshopmongo/services/UserService.java
+++ src/main/java/com/andreygrosbelli/workshopmongo/services/UserService.java
@@ -1,6 +1,7 @@
 package com.andreygrosbelli.workshopmongo.services;
 
 import com.andreygrosbelli.workshopmongo.domain.User;
+import com.andreygrosbelli.workshopmongo.dto.UserDTO;
 import com.andreygrosbelli.workshopmongo.repositories.UserRepository;
 import com.andreygrosbelli.workshopmongo.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,5 +23,13 @@ public class UserService {
     public User findById(String id) {
         Optional<User> obj = repo.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+    public User insert(User obj) {
+        return repo.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDto){
+        return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
     }
 }
