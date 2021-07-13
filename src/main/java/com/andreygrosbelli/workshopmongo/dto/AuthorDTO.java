416061f23671b71cdbe3527579fdf8aeba8249dd
Andrey Grosbelli <agrosbelli@yahoo.com.br>
2021-07-13 20:01:33 -0300

    Projeção dos dados do autor com DTO

---
+++ src/main/java/com/andreygrosbelli/workshopmongo/dto/AuthorDTO.java
@@ -0,0 +1,39 @@
+package com.andreygrosbelli.workshopmongo.dto;
+
+import com.andreygrosbelli.workshopmongo.domain.User;
+
+import java.io.Serializable;
+
+public class AuthorDTO implements Serializable {
+
+    private String id;
+    private String name;
+
+    public AuthorDTO() {}
+
+    public AuthorDTO(User obj) {
+        id = obj.getId();
+        name = obj.getName();
+    }
+
+    public AuthorDTO(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
