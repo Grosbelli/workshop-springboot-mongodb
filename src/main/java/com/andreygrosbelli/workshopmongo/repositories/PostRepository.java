c7d4d75e43e53d3769dcb7b658766930b6e493ac
Andrey Grosbelli <agrosbelli@yahoo.com.br>
2021-07-14 19:06:42 -0300

    Consulta simples com @Query

--- src/main/java/com/andreygrosbelli/workshopmongo/repositories/PostRepository.java
+++ src/main/java/com/andreygrosbelli/workshopmongo/repositories/PostRepository.java
@@ -2,6 +2,7 @@ package com.andreygrosbelli.workshopmongo.repositories;
 
 import com.andreygrosbelli.workshopmongo.domain.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,5 +10,8 @@ import java.util.List;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 
+    //https://docs.mongodb.com/manual/reference/operator/query/regex/
+    @Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+    List<Post> searchTitle(String text);
     List<Post> findByTitleContainingIgnoreCase(String text);
 }
